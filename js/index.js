f31c09745f5bb96bfa298a291a52095a8b77eb71
Javier Neira <javier.neira@web.de>
2022-12-01 22:35:31 +0100

    incomplete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
-window.onload = () => {
+/*window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
   function startGame() {}
+};*/
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+ img.src = "/images/road.png";
+
+
+  function startGame() {
+    ctx.clearRect(0, 0, 1500, 1700);
+    ctx.drawImage(img, 0, 0, 500, 700);
+    console.log("Just do anything. Literally whatever.")
+  }
+  window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+      startGame();
+    };
 };
